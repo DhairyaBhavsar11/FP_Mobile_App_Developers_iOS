f00aae87d491b802e64bb53dadf7142e0d3eb3c8
DhairyaBhavsar11 <c0816994@mylambton.ca>
2022-05-15 22:01:09 -0600

    UI Making Started ( add new view controller and navigation controller to swipes between pages)

---
+++ Travel Diries/DetailPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailPageViewController.swift
+//  Travel Diries
+//
+//  Created by dhairya bhavsar on 2022-05-15.
+//
+
+import UIKit
+
+class DetailPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "first screen"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
