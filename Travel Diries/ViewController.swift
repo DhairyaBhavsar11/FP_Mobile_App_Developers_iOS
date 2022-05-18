4d968c569a37c29a950b7a0b15283d1f275436a0
DhairyaBhavsar11 <c0816994@mylambton.ca>
2022-05-17 21:38:45 -0600

    Animation Completed

--- Travel Diries/ViewController.swift
+++ Travel Diries/ViewController.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private let imageView: UIImageView = {
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 180, height: 180))
+    var imageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 150, height: 150))
         imageView.image = UIImage(named: "Logo")
         return imageView
     }()
@@ -19,20 +19,23 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.addSubview(imageView)
+        DispatchQueue.main.asyncAfter(deadline: .now()+3){
+            self.performSegue(withIdentifier: "MainScreen", sender: self)
+        }
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         imageView.center = view.center
         
-        DispatchQueue.main.asyncAfter(deadline: .now()+0.5, execute: {
+        DispatchQueue.main.asyncAfter(deadline: .now()+1.5){
             self.animate()
-        })
+        }
     }
     
     private func animate(){
-        UIView.animate(withDuration: 1, animations: {
-            let size = self.view.frame.size.width * 3
+        UIView.animate(withDuration: 1) {
+            let size = self.view.frame.size.width * 2
             let diffx = size - self.view.frame.size.width
             let diffy = self.view.frame.size.height - size
             
@@ -40,16 +43,7 @@ class ViewController: UIViewController {
             self.imageView.frame = CGRect(x: -(diffx/2), y: diffy/2, width: size, height: size)
             
             self.imageView.alpha = 0
-        }, completion: {done in
-            if done{
-                DispatchQueue.main.asyncAfter(deadline: .now()+2.0, execute: {
-                    let viewController = HomePageViewController()
-                    viewController.modalTransitionStyle = .crossDissolve
-                    viewController.modalPresentationStyle = .fullScreen
-                    self.present(viewController, animated: true)
-                })
-            }
-        })
+        }
     }
     
 }
